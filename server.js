862964ee761dba8b9ae212b7d4e659bb23906347
Ross Hadden <rosshadden@gmail.com>
2011-11-18 11:29:45 -0500

    Sockets->JS. Got rid of all socket.set/get() commands in favor of storing the information in players[]; Renamed some variables (sockets->io, client->socket, main->serve, etc); Got rid of now-useless commented code blocks;

--- server.js
+++ server.js
@@ -53,31 +53,25 @@ var fs = require('fs'),
 					break;
 				case 'party':
 					data = [];
-					players[username].get('leader',function(err,leader){
-						for(person in parties[leader]){
-							data.push(person);
-						}
-					});
+					for(person in parties[players[username].leader]){
+						data.push(person);
+					}
 					break;
 				case 'invite':
 					if(players[url.query.user]){
-						players[username].get('user',function(err,host){
-							console.log('invite:',host,'->',url.query.user);
-							players[url.query.user].emit('party.invite',host);
-						});
+						console.log('invite:',players[username].user,'->',url.query.user);
+						players[url.query.user].socket.emit('party.invite',players[username].user);
 					}
 					break;
 				case 'accept':
-					players[username].get('user',function(err,guest){
-						console.log('accept:',url.query.user,'->',guest);
-						if(!parties[url.query.user]){
-							parties[url.query.user] = {};
-							parties[url.query.user][url.query.user] = players[url.query.user];
-							players[url.query.user].set('leader',url.query.user);
-						}
-						parties[url.query.user][guest] = players[guest];
-						players[guest].set('leader',url.query.user);
-					});
+					console.log('accept:',url.query.user,'->',players[username].user);
+					if(!parties[url.query.user]){
+						parties[url.query.user] = {};
+						parties[url.query.user][url.query.user] = players[url.query.user];
+						players[url.query.user].leader = url.query.user;
+					}
+					parties[url.query.user][players[username].user] = players[players[username].user];
+					players[players[username].user].leader = url.query.user;
 					break;
 				default:
 					data = 'default';
@@ -86,26 +80,10 @@ var fs = require('fs'),
 			response.end(JSON.stringify(data));
 		});
 	},
-	main = function(){
+	serve = function(){
 		init();
-		var socket = require('socket.io').listen(app);
-		/*server = http.createServer(function(request,response){
-			var rs,
-				route = router.route(request.url);
-			response.writeHead(200,{
-				'Content-Type':	route.mime
-			});
-			if(route.type === 'http'){
-				rs = fs.createReadStream(route.path,response);
-				util.pump(rs,response);
-			}else if(route.type === 'ajax'){
-				response.end(JSON.stringify(ajax[route.url.split('/')[2]](route.query)));
-			}else{
-				console.log('Invalid Request.');
-				response.end('Invalid Request.');
-			}
-		})*/
-		socket.set('log level',1);
+		var io = require('socket.io').listen(app);
+		io.set('log level',1);
 		world.collision.render({
 			map: {
 				x:	1,
@@ -115,7 +93,7 @@ var fs = require('fs'),
 		console.log('http://localhost:%d | %s',+(process.argv[2] || 4),app.settings.env);
 		app.listen(+(process.argv[2] || 4));
 		
-		socket.set('authorization',function(data,accept){
+		io.set('authorization',function(data,accept){
 			if(data.headers.cookie){
 				data.cookie = parseCookie(data.headers.cookie);
 				data.sessionID = data.cookie['express.sid'];
@@ -136,264 +114,210 @@ var fs = require('fs'),
 			   return accept('No cookie transmitted.',false);
 			}
 		});
-		socket.sockets.on('connection',function(client){
-			var me = client.handshake.sessionID,
-				protocol = Object.keys(client.handshake.session.auth)[0],
-				username = protocol + '|' + client.handshake.session.auth[protocol].user.id;
-			client.on('login',function(){
-				players[username] = client;
-				console.log('LOG:','Player ' + ++numClients + ' logged in:',client.handshake.address.address,username);
-				client.emit('login',username);
-				client.set('user',username);
-				client.set('position',{
-					at: {
-						x:	300,
-						y:	200
-					},
-					map: {
-						x:	1,
-						y:	1
+		io.sockets.on('connection',function(socket){
+			var me = socket.handshake.sessionID,
+				protocol = Object.keys(socket.handshake.session.auth)[0],
+				username = protocol + '|' + socket.handshake.session.auth[protocol].user.id;
+			socket.on('login',function(){
+				players[username] = {
+					user:		username,
+					socket:		socket,
+					inventory:	[],
+					position: {
+						at: {
+							x:	300,
+							y:	200
+						},
+						map: {
+							x:	1,
+							y:	1
+						}
 					}
-				});
-				client.set('inventory',[]);
-				client.broadcast.emit('player.update',{
+				};
+				console.log('LOG:','Player ' + ++numClients + ' logged in:',socket.handshake.address.address,username);
+				socket.emit('login',username);
+				socket.broadcast.emit('player.update',{
 					user:	username,
 					path:	[{x:12,y:8}]
 				});
 				for(person in players){
-					players[person].get('user',function(err,user){
-						players[person].get('position',function(err,position){
-							if(username === user && players[person] !== client){
-								//	Theoretically this is how we handle reentry.
-								//	Or... just use sessions.
-							}
-							if(players[person] !== client && position && position.map.x === 1 && position.map.y === 1){
-								client.emit('player.update',{
-									user:	user,
-									path:	[world.toGrid(position.at)]
-								});
-							}
+					if(username === players[person].user && players[person].socket !== socket){
+						//	Theoretically this is how we handle reentry.
+						//	Or... just use sessions.
+					}
+					if(players[person].socket !== socket && players[person].position && players[person].position.map.x === 1 && players[person].position.map.y === 1){
+						socket.emit('player.update',{
+							user:	players[person].user,
+							path:	[world.toGrid(players[person].position.at)]
 						});
-					});
+					}
 				}
-				client.on('player.move',function(data){
-					client.get('position',function(err,position){
-						client.get('user',function(err,user){
-							var collision = world.collision.check(data,position);
-							if(!collision.vacant){
-								client.emit('player.move',false);
-							}else{
-								position.at.x = data.x;
-								position.at.y = data.y;
-								data = {
-									user:	user,
-									map:	position.map,
-									path:	pathfinder(
-										world.map(position),
-										world.toGrid(data.old),
-										world.toGrid(data),
-										'manhattan'
-									),
-									entity:	collision.vacant,
-									door:	collision.door
-								};
-								if(collision.vacant === 'item' || collision.vacant === 'door'){
-									data.to = data.path[data.path.length - 1];
-									data.path = data.path.slice(0,-1);
-								}
-								for(person in players){
-									players[person].get('position',function(err,remotePosition){
-										if(
-											players[person] !== client
-										&&	remotePosition
-										&&	position.map.x === remotePosition.map.x
-										&&	position.map.y === remotePosition.map.y
-										&&(		!position.instance
-											&&	!remotePosition.instance
-											||	position.instance
-											&&	remotePosition.instance
-											&&	position.instance.x === remotePosition.instance.x
-											&&	position.instance.y === remotePosition.instance.y
-										)
-										){
-											players[person].emit('player.update',data);
-										}
-									});
-								}
-								data.instance = position.instance;
-								client.set('position',position);
-								client.emit('player.move',data);
-							}
-						});
-					});
-				});
-			});
-			client.on('player.changeMap',function(data){
-				client.get('position',function(err,position){
-					client.get('user',function(err,user){
-						var old = {
-							map: {
-								x:	position.map.x,
-								y:	position.map.y
-							},
-							instance:	position.instance
+				socket.on('player.move',function(data){
+					var collision = world.collision.check(data,players[username].position);
+					if(!collision.vacant){
+						socket.emit('player.move',false);
+					}else{
+						players[username].position.at.x = data.x;
+						players[username].position.at.y = data.y;
+						data = {
+							user:	players[username].user,
+							map:	players[username].position.map,
+							path:	pathfinder(
+								world.map(players[username].position),
+								world.toGrid(data.old),
+								world.toGrid(data),
+								'manhattan'
+							),
+							entity:	collision.vacant,
+							door:	collision.door
 						};
-						if(data.door){
-							if(data.door.instance){
-								position.instance = data.door.instance;
-							}else{
-								delete position.instance;
-							}
-							position.path = [data.door.to];
-						}else if(world.collision.onEdge(
-							world.toGrid(position.at),
-							data.dir
-						)){
-							switch(data.dir){
-								case 'N':
-									position.map.y -= 1;
-									position.at.y = 375;
-									break;
-								case 'E':
-									position.map.x += 1;
-									position.at.x = 0;
-									break;
-								case 'S':
-									position.map.y += 1;
-									position.at.y = 0;
-									break;
-								case 'W':
-									position.map.x -= 1;
-									position.at.x = 575;
-									break;
-							}
-							position.path = [world.toGrid(position.at)];
+						if(collision.vacant === 'item' || collision.vacant === 'door'){
+							data.to = data.path[data.path.length - 1];
+							data.path = data.path.slice(0,-1);
 						}
-						position.path[0].dir = data.dir;
-						client.set('position',position);
-						client.emit('player.changeMap',position);
-						client.emit('player.warp',position);
-						world.collision.render(position);
 						for(person in players){
-							players[person].get('position',function(err,remotePosition){
-								if(
-									players[person] !== client
-								&&  old && remotePosition
-								&&	old.map.x === remotePosition.map.x
-								&&	old.map.y === remotePosition.map.y
-								){
-									players[person].emit('player.exitMap',user);
-								}else if(
-									players[person] !== client
-								&&  position && remotePosition
-								&&	position.map.x === remotePosition.map.x
-								&&	position.map.y === remotePosition.map.y
-								&&(		!position.instance
-									&&	!remotePosition.instance
-									||	position.instance
-									&&	remotePosition.instance
-									&&	position.instance.x === remotePosition.instance.x
-									&&	position.instance.y === remotePosition.instance.y
-								)
-								){
-									position.user = user;
-									players[person].emit('player.update',position);
-									
-								}
-							});
+							if(
+								players[person].socket !== socket
+							&&	players[person].position
+							&&	players[username].position.map.x === players[person].position.map.x
+							&&	players[username].position.map.y === players[person].position.map.y
+							&&(		!players[username].position.instance
+								&&	!players[person].position.instance
+								||	players[username].position.instance
+								&&	players[person].position.instance
+								&&	players[username].position.instance.x === players[person].position.instance.x
+								&&	players[username].position.instance.y === players[person].position.instance.y
+							)
+							){
+								players[person].socket.emit('player.update',data);
+							}
 						}
-					});
-				});
-			});
-			client.on('players.update',function(){
-				client.get('position',function(err,position){
-					for(person in players){
-						players[person].get('user',function(err,user){
-							players[person].get('position',function(err,remotePosition){
-								if(
-									players[person] !== client
-								&&  position && remotePosition
-								&&	position.map.x === remotePosition.map.x
-								&&	position.map.y === remotePosition.map.y
-								&&(		!position.instance
-									&&	!remotePosition.instance
-									||	position.instance
-									&&	remotePosition.instance
-									&&	position.instance.x === remotePosition.instance.x
-									&&	position.instance.y === remotePosition.instance.y
-								)
-								){
-									client.emit('player.update',{
-										user:	user,
-										path:	[world.toGrid(remotePosition.at)]
-									});
-								}
-							});
-						});
+						data.instance = players[username].position.instance;
+						socket.emit('player.move',data);
 					}
 				});
 			});
-			client.on('player.openChest',function(){
-				client.get('user',function(err,user){
-					client.get('position',function(err,position){
-						var i,response,
-							positionFix = world.toGrid(position.at),
-							item = world.map(position)[positionFix.x][positionFix.y];
-						if(!item.data){
-							item.data = {};
-						}
-						if(!item.data.opened){
-							item.data.opened = [];
-						}
-						if(item.data.opened.indexOf(user) === -1){
-							item.data.opened.push(user);
-							response = 'Woot, you got an item.';
-							client.get('inventory',function(err,inventory){
-								for(i = 0; i < item.data.contents.length; i++){
-									inventory.push(item.data.contents[i]);
-								}
-								client.set('inventory',inventory);
-							});
-						}else{
-							response = 'No such luck.';
-						}
-						client.emit('player.openChest',response);
-					});
-				});
+			socket.on('player.changeMap',function(data){
+				var old = {
+					map: {
+						x:	players[username].position.map.x,
+						y:	players[username].position.map.y
+					},
+					instance:	players[username].position.instance
+				};
+				if(data.door){
+					if(data.door.instance){
+						players[username].position.instance = data.door.instance;
+					}else{
+						delete players[username].position.instance;
+					}
+					players[username].position.path = [data.door.to];
+				}else if(world.collision.onEdge(
+					world.toGrid(players[username].position.at),
+					data.dir
+				)){
+					switch(data.dir){
+						case 'N':
+							players[username].position.map.y -= 1;
+							players[username].position.at.y = 375;
+							break;
+						case 'E':
+							players[username].position.map.x += 1;
+							players[username].position.at.x = 0;
+							break;
+						case 'S':
+							players[username].position.map.y += 1;
+							players[username].position.at.y = 0;
+							break;
+						case 'W':
+							players[username].position.map.x -= 1;
+							players[username].position.at.x = 575;
+							break;
+					}
+					players[username].position.path = [world.toGrid(players[username].position.at)];
+				}
+				players[username].position.path[0].dir = data.dir;
+				socket.emit('player.changeMap',players[username].position);
+				socket.emit('player.warp',players[username].position);
+				world.collision.render(players[username].position);
+				for(person in players){
+					if(
+						players[person].socket !== socket
+					&&  old && players[person].position
+					&&	old.map.x === players[person].position.map.x
+					&&	old.map.y === players[person].position.map.y
+					){
+						players[person].socket.emit('player.exitMap',players[username].user);
+					}else if(
+						players[person].socket !== socket
+					&&  players[username].position && players[person].position
+					&&	players[username].position.map.x === players[person].position.map.x
+					&&	players[username].position.map.y === players[person].position.map.y
+					&&(		!players[username].position.instance
+						&&	!players[person].position.instance
+						||	players[username].position.instance
+						&&	players[person].position.instance
+						&&	players[username].position.instance.x === players[person].position.instance.x
+						&&	players[username].position.instance.y === players[person].position.instance.y
+					)
+					){
+						players[username].position.user = players[username].user;
+						players[person].socket.emit('player.update',players[username].position);
+						
+					}
+				}
 			});
-			client.on('inventory.get',function(){
-				client.get('inventory',function(err,inventory){
-					client.emit('inventory.get',inventory);
-				});
+			socket.on('players.update',function(){
+				for(person in players){
+					if(
+						players[person].socket !== socket
+					&&  players[username].position && players[person].position
+					&&	players[username].position.map.x === players[person].position.map.x
+					&&	players[username].position.map.y === players[person].position.map.y
+					&&(		!players[username].position.instance
+						&&	!players[person].position.instance
+						||	players[username].position.instance
+						&&	players[person].position.instance
+						&&	players[username].position.instance.x === players[person].position.instance.x
+						&&	players[username].position.instance.y === players[person].position.instance.y
+					)
+					){
+						socket.emit('player.update',{
+							user:	players[person].user,
+							path:	[world.toGrid(players[person].position.at)]
+						});
+					}
+				}
 			});
-			client.on('disconnect',function(person){
+			socket.on('player.openChest',function(){
+					var i,response,
+						positionFix = world.toGrid(players[username].position.at),
+						item = world.map(players[username].position)[positionFix.x][positionFix.y];
+					if(!item.data){
+						item.data = {};
+					}
+					if(!item.data.opened){
+						item.data.opened = [];
+					}
+					if(item.data.opened.indexOf(players[username].user) === -1){
+						item.data.opened.push(players[username].user);
+						response = 'Woot, you got an item.';
+						for(i = 0; i < item.data.contents.length; i++){
+							players[username].inventory.push(item.data.contents[i]);
+						}
+					}else{
+						response = 'No such luck.';
+					}
+					socket.emit('player.openChest',response);
+			});
+			socket.on('inventory.get',function(){
+				socket.emit('inventory.get',players[username].inventory);
+			});
+			socket.on('disconnect',function(person){
 				delete players[person];
-				client.get('user',function(err,user){
-					console.log('LOG:','Player ' + numClients-- + ' logged out:',client.handshake.address.address,user);
-					client.broadcast.emit('logoff',user);
-				});
+				console.log('LOG:','Player ' + numClients-- + ' logged out:',socket.handshake.address.address,players[username].user);
+				socket.broadcast.emit('logoff',players[username].user);
 			});
 		});
-	},
-	ajax = {
-		invite: function(guest){
-			if(players[guest.user]){
-				/*me.get('user',function(err,host){
-					console.log('invite:',host,'->',guest.user);
-					players[guest.user].emit('party.invite',host);
-				});*/
-			}
-		},
-		accept: function(host){
-			/*me.get('user',function(err,guest){
-				console.log('accept:',host.user,'->',guest);
-				if(!parties[host.user]){
-					parties[host.user] = {};
-					parties[host.user][host.user] = players[host.user];
-				}
-				parties[host.user][guest] = players[guest];
-				console.log(parties);
-			});*/
-		}
 	};
-exports.start = main;
+exports.start = serve;
